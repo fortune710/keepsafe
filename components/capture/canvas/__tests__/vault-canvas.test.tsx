a3cd1abc332ceaacd658e059b8a5ee372887e1c1
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2025-09-27 00:51:43 -0400

    fixed jest configuration

--- components/capture/canvas/__tests__/vault-canvas.test.tsx
+++ components/capture/canvas/__tests__/vault-canvas.test.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { render } from "@testing-library/react-native";
 import VaultCanvas from "@/components/capture/canvas/vault-canvas";
 import { MediaType } from "@/types/media";
 import { RenderedMediaCanvasItem } from "@/types/capture";
 
 describe("Vault Canvas Tests", () => {
-    it("renders only image when there are no canvas items", () => {
+    test("renders only image when there are no canvas items", () => {
         const expectedTestId = "vault-canvas-image";
 
         const uri = "test-uri";
